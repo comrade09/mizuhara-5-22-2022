a823021a33f6431dccb35f4900126b2e2337ea31
Sasta Dev <89509397+SastaDev@users.noreply.github.com>
2022-05-28 01:59:04 +0530

    Update NineAnimeDev.py

--- Mizuharabot/modules/NineAnimeDev.py
+++ Mizuharabot/modules/NineAnimeDev.py
@@ -1,25 +1,27 @@
 from Mizuharabot import telethn
 from Mizuharabot.events import register
+from telethon import Button
 
 from animedev import client as AnimeDevClient, exceptions
 
 @register(pattern='/search')
 async def animedev_function(event):
-    await event.reply(str(event.message.message))
     anime_name = event.message.message.split()
     if len(anime_name) <= 1:
         await event.reply('Please enter the anime name.\n\n<b>Example:</b>\n/search Doraemon.')
         return
     try:
         anime = AnimeDevClient.search(' '.join(anime_name[1:]))
+        await event.reply(str(anime))
     except exceptions.NotFound:
         await event.reply('Anime not found.')
         return
     except Exception as e:
-        await event.reply(f'[ERROR]: {e}\n\nPlease report this at @Shinobu_Support.')
+        await event.reply(f'[ERROR]: Please report this at @Shinobu_Support.\n\nERROR:\n{e}')
         return
+    await event.reply('lol')
     msg_text = f'''
 Anime Title: {anime['AnimeTitle']}
     '''
     buttons = [[Button.url('Download Link', anime['AnimeLink'])], [Button.url('Search Query', anime['Search_Query'])]]
-    await telethn.send_file(event.chat_id, anime['AnimeImg'], caption=msg_text)
+    await telethn.send_file(event.chat_id, anime['AnimeImg'], caption=msg_text, parse_mode='html')
