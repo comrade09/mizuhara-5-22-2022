f53571d947eb9c207702573dfe8486f68986a3f0
Sasta Dev <89509397+SastaDev@users.noreply.github.com>
2022-05-28 01:49:45 +0530

    Update NineAnimeDev.py

--- Mizuharabot/modules/NineAnimeDev.py
+++ Mizuharabot/modules/NineAnimeDev.py
@@ -5,18 +5,18 @@ from animedev import client as AnimeDevClient, exceptions
 
 @register(pattern='/search')
 async def animedev_function(event):
-    await event.reply(str(event.message))
-    anime_name = event.message.split()
+    await event.reply(str(event.message.message))
+    anime_name = event.message.message.split()
     if len(anime_name) <= 1:
-        await message.reply_text('Please enter the anime name.\n\n<b>Example:</b>\n/search Doraemon.')
+        await event.reply('Please enter the anime name.\n\n<b>Example:</b>\n/search Doraemon.')
         return
     try:
         anime = AnimeDevClient.search(' '.join(anime_name[1:]))
     except exceptions.NotFound:
-        await message.reply_text('Anime not found.')
+        await event.reply('Anime not found.')
         return
     except Exception as e:
-        await message.reply_text(f'[ERROR]: {e}\n\nPlease report this at @Shinobu_Support.')
+        await event.reply(f'[ERROR]: {e}\n\nPlease report this at @Shinobu_Support.')
         return
     msg_text = f'''
 Anime Title: {anime['AnimeTitle']}
